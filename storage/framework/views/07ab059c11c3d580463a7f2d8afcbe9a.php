7b5c9f1952acfb25758eafaefd6dd0a6f375e224
Gourav Kumar <sanamtaneja23@gmail.com>
2025-09-09 13:21:16 +0000

    code chnages

---
+++ storage/framework/views/07ab059c11c3d580463a7f2d8afcbe9a.php
@@ -0,0 +1,38 @@
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/vendors.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/tables/datatable/dataTables.bootstrap5.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/tables/datatable/responsive.bootstrap5.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/tables/datatable/buttons.bootstrap5.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/editors/quill/katex.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/editors/quill/monokai-sublime.min.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/editors/quill/quill.snow.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/editors/quill/quill.bubble.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/vendors/css/forms/select/select2.min.css')); ?>">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/bootstrap.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/bootstrap-extended.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/colors.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/components.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/themes/dark-layout.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/themes/bordered-layout.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/themes/semi-dark-layout.css')); ?>">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/core/menu/menu-types/vertical-menu.css')); ?>">
+      <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/js/jquery-ui.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('app-assets/css/pages/app-email.css')); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('assets/css/style.css')); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('assets/css/iconsheet.css')); ?>">
+    <!-- END: Custom CSS--> 
+
+    <!--summernote-->
+    <link href="<?php echo e(asset('app-assets/summernote/summernote-lite.min.css')); ?>" rel="stylesheet"><?php /**PATH /var/www/html/omega_presecen360/resources/views/layouts/partials/css.blade.php ENDPATH**/ ?>
