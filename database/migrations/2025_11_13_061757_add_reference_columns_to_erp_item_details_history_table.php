9ec8bb5dd56a80fb8398be20e9c6a876c68ab279
Jogender <Jogenderdelaine@gmail.com>
2025-11-13 13:48:22 +0530

    migration file for item details

---
+++ database/migrations/2025_11_13_061757_add_reference_columns_to_erp_item_details_history_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('erp_item_details_history', function (Blueprint $table) {
+            $table->string('reference_party_no')->nullable();
+            $table->date('reference_party_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('erp_item_details_history', function (Blueprint $table) {
+            $table->dropColumn('reference_party_no');
+            $table->dropColumn('reference_party_date');
+        });
+    }
+};
